5318415217c744cdaabb139b39d75173d44ed506
Mateusz Hinca <s20597@pjwstk.edu.pl>
2020-03-15 18:39:04 +0100

    Rectangle

---
+++ ZAJ2/Zad6/Rectangle.java
@@ -0,0 +1,26 @@
+package Zad6;
+
+/**
+ * summary: Rectangle class
+ * author: Mateusz Hinca
+ **/
+
+public class Rectangle{
+    private double length;
+    private double width;
+
+    public Rectangle(double length,double width){
+        this.length=length;
+        this.width=width;
+    }
+
+    public double calcField(){
+        return length*width;
+    }
+    public double calcCircuit(){
+        return 2*length+2*width;
+    }
+    public double calcDiagonal(){
+        return Math.sqrt((width*width)+(length*length))
+    }
+}
